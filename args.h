072224aebc87f341a81d2c888a9a4548531ff092
Doggo <fearlessdoggo21@vivaldi.net>
2021-11-04 18:42:22 -0500

    update args.h

--- args.h
+++ args.h
@@ -6,17 +6,7 @@
 typedef struct args {
 	const char *name, *passwd, *amount, *auth, *time, *server;
 	const Endpoint *ep;
-} ArgsR;
-
-typedef struct args {
-	const Endpoint *ep;
-	const char *name, *passwd, *auth, *server;
-	long amount, time;
 } Args;
-
-typedef struct args_ptr {
-	char *name, *passwd, *amount, *time, *user, *admin, *server;
-} ArgsPtr;
 
 void parse_args(Args *args, const char **argv);
 
